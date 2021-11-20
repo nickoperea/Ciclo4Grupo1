089927739be2103f85734d7896ca1cf0e0211ac2
nickoperea <nickoperea@gmail.com>
2021-11-19 20:51:56 -0500

    Login 3.0

--- InventariApp2/app/src/main/java/com/nickoperea/inventariapp/MainActivity.kt
+++ InventariApp2/app/src/main/java/com/nickoperea/inventariapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_InventariApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
